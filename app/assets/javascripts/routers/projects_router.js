e2cbc799fa29103de4d17148ad3f98458e173c94
Adrian Cann <adriancann@adriancann.com>
2014-07-10 21:58:27 -0400

    adds project show view and template

--- app/assets/javascripts/routers/projects_router.js
+++ app/assets/javascripts/routers/projects_router.js
@@ -1,7 +1,6 @@
 Maddie.Routers.ProjectsRouter = Backbone.Router.extend({
 	initialize: function(options) {
 		this.$rootEl = options.$rootEL;
-		// Backbone.history.navigate("/projects", {trigger: true})
 	},
 
 	routes: {
@@ -27,6 +26,7 @@ Maddie.Routers.ProjectsRouter = Backbone.Router.extend({
 	},
 	
   show: function (id) {
+		var project = Maddie.Collections.projects.get(id)
     var showView = new Maddie.Views.ProjectShow({
       model: project
     });
---
+++ app/assets/javascripts/views/projects/show.js
@@ -0,0 +1,33 @@
+Maddie.Views.ProjectShow = Backbone.View.extend({
+	
+	initialize: function(options){
+		// options.collection = this.collection
+		this.model = options.model;
+	},
+
+  template: JST['projects/show'],
+	
+	events: {
+		"click .back": "back"
+	},
+	
+	render: function() {
+		
+		var renderedContent = this.template({
+			model: this.model
+		})
+		
+		this.$el.html(renderedContent);
+		return this;
+		
+		
+	}, 
+	
+	back: function(){
+		
+		window.Maddie.appRouter.navigate("", {trigger: true})
+		// console.log("here")
+		
+	}
+
+});
