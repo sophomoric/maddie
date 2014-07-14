fdd92e15486468e83037d5c6cca79ee429b0e57a
Adrian Cann <adriancann@adriancann.com>
2014-07-14 13:38:19 -0400

    adds photos model with attached file

--- db/migrate/20140703225339_add_avatar_columns_to_projects.rb
+++
@@ -1,9 +0,0 @@
-class AddAvatarColumnsToProjects < ActiveRecord::Migration
-  def self.up
-    add_attachment :projects, :avatar
-  end
-
-  def self.down
-    remove_attachment :projects, :avatar
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710143154) do
+ActiveRecord::Schema.define(version: 20140714173312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20140710143154) do
     t.integer "medium_id"
   end
 
+  create_table "photos", force: true do |t|
+    t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
+
   create_table "projects", force: true do |t|
     t.string   "title"
     t.string   "description"
@@ -37,10 +47,6 @@ ActiveRecord::Schema.define(version: 20140710143154) do
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   add_index "projects", ["year"], name: "index_projects_on_year", using: :btree
