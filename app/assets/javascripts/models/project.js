a2c2afb492365ba07c1a1b413765df4b999df774
Adrian Cann <adriancann@adriancann.com>
2014-07-14 21:04:53 -0400

    adds media method to comment model

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -1,3 +1,13 @@
 Maddie.Models.Project = Backbone.Model.extend({
 	urlRoot: "/api/projects"
+	
+	media: function() {
+		if (!this._media){
+			this._media = new Maddie.Collections.Media([], {
+				project: this
+			})
+		}
+		
+		return this._media;
+	}
 });
