463425c0ee2e34c4b36b3474706e21719b5edee3
Adrian Cann <adriantcann@gmail.com>
2014-08-31 19:42:44 -0400

    adds logic to seed from assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,18 @@ all_tags.count.times do |number|
   Medium.create({:name => all_tags[number], :avatar => seed_image(avatar[number])})
 end
 
-# Medium.create(name: "rails pic test", avatar: File.open("/Users/adriancann/Downloads/solutions-master/w7/w7d1/phototaggr/app/assets/images/rails.png"))
-#seed in production
+Photo.destroy_all
+Project.destroy_all
+def seed_image(subfolder, file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/ArtPortfolio/Adrian/" + subfolder + "/#{file_name}"))
+end
+
+Dir.entries("app/assets/images/ArtPortfolio/Adrian").each do |entry|
+  next if entry[0] == "."
+  project = Project.create({title: entry, description: "blah blah this is art. you should probably change the default description"})
+  Dir.entries("app/assets/images/ArtPortfolio/Adrian" + "/" + entry).each do |picture|
+    next if picture[0] == "."
+    photo = project.photos.new(avatar: seed_image(entry, picture))
+    photo.save
+  end
+end
