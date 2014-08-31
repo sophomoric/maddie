99a41ea48c9a7d93d877dadfce688ee609069b6e
Adrian Cann <adriantcann@gmail.com>
2014-08-31 15:44:05 -0400

    Revert "swaps over to mysql"
    This reverts commit 140423fea6c7c29a9719bd11efa510e3d6ae7799.

--- README.md
+++ README.md
@@ -15,4 +15,3 @@
 - Needs Contact info page (maybe a form to email)
 - Links to purchase
 
-- figure out scripts to deploy on paid hosting
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140720142307) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "media", force: true do |t|
     t.string   "name"
     t.string   "description"
