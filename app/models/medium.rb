15e65a13d5d6308cc0d5a4ddaeef701762ced78d
Adrian Cann <adriantcann@gmail.com>
2014-11-29 08:45:26 -0500

    more readable has_attached_file option hash

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -2,7 +2,12 @@ class Medium < ActiveRecord::Base
   has_and_belongs_to_many :projects
   
   before_validation :make_url_image
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :auto_bg => "auto_bg" }, :default_url => "http://s3.amazonaws.com/rebulba_dev/users/avatars/000/000/007/original/puppy.jpg?1398551275"
+  has_attached_file :avatar,
+                    :styles => {
+                                 :medium => "300x300>",
+                                 :thumb => "100x100>",
+                                 :auto_bg => "auto_bg" 
+                               }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
   
