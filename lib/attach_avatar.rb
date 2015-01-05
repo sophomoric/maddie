d32c5159c67216b42305c65177ecef215de2a597
Adrian Cann <acann@booster.com>
2015-01-05 06:17:51 -0500

    break up long lines

--- lib/attach_avatar.rb
+++ lib/attach_avatar.rb
@@ -1,4 +1,5 @@
 module AttachAvatar
+  FILE_FORMATS = [/png\Z/, /jpe?g\Z/, /PNG\Z/, /JPE?G\Z/]
   def self.included(base)
     base.before_validation :make_url_image
     base.has_attached_file :avatar, processors: [:cropper],
@@ -7,8 +8,11 @@ module AttachAvatar
                                   :thumb => "100x100>",
                                   :auto_bg => "auto_bg"
                                   }
-    base.validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-    base.validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/, /PNG\Z/, /JPE?G\Z/]
+    base.validates_attachment_content_type :avatar,
+                                           :content_type => /\Aimage\/.*\Z/
+
+    base.validates_attachment_file_name :avatar,
+                                        :matches => FILE_FORMATS
   end
 
   def make_url_image
