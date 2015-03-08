997ae5bea21277b134a58ae115b6feb5cb17cc77
Adrian Cann <acann@booster.com>
2015-03-07 23:31:23 -0500

    Menu for user. remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Maddie::Application.routes.draw do
   resources :photos, only: [:update] do
   end
 
-  resources :projects
+  resources :projects, except: [:index]
 
   resources :pages, only: [:index, :create, :update, :destroy]
 
