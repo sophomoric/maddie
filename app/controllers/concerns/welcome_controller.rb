accfc7b9f6ab9d8890880746b8113d49aac542e1
Adrian Cann <acann@booster.com>
2015-05-16 14:38:04 -0400

    404 rather than 500 error when page not found
    routes of the format "/page/*" would 500 if the * url key did not exist.
    Page.find_by_attribute(*) does not raise active record not found (while
    Page.find(id) does.

--- app/controllers/concerns/welcome_controller.rb
+++ app/controllers/concerns/welcome_controller.rb
@@ -5,5 +5,6 @@ class WelcomeController < ApplicationController
 
   def show
     @page = Page.find_by_url_key(params[:url_key])
+    raise ActiveRecord::RecordNotFound unless @page
   end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe WelcomeController do
       expect(response).to render_template(:index)
     end
   end
+
+  describe "GET show" do
+    context "page name exists" do
+      it "renders the show page" do
+        page = create :page
+
+        get :show, url_key: page.url_key
+
+        expect(response).to render_template(:show)
+      end
+    end
+
+    context "page url_key does not exist" do
+      it "raises a 404" do
+        expect do
+          get :show, url_key: "not_found"
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+  end
 end
