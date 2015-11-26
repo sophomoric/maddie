7f8555b9f6c687777a62f3ed846a646f2fc555cf
Adrian Cann <adriantcann@gmail.com>
2015-11-26 13:31:15 -0500

    Automatically resize textareas to fit contents
    Styling a page can be annoying when the body becomes large. Scrolling to
    the preview and back to the input would be delayed by scrolling to the
    bottom of the input.

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'uglifier', '>= 1.3.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'autosize-rails'
 gem 'bootstrap-sass'
 
 group :doc do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require jquery.autosize
+//= require textarea
 //= require preview
 //= require image_tags
