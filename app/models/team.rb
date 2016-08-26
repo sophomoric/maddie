cebe864453ddc894ca641bc419d5e7384dca4ccc
Adrian Cann <adriantcann@gmail.com>
2016-08-26 14:30:38 -0400

    Give Team fun names

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,13 @@
 class Team
+  ADJECTIVES = %w(
+    B-to-the-S-to-the- Exploding Shooting Super Deadly Fighting Lightening-quick
+    Fiery Hideous
+  )
+  NOUNS = %w(
+    Monsters Phoenixes Dragons Longshots Blades Spirits Warriors Breadman Mages 
+    Programmers Paige-Pipers
+  )
+
   def initialize(names)
     @names = names.split(" ").shuffle!
   end
@@ -15,9 +24,13 @@ class Team
     "<p>" + text(names) + "</p>"
   end
 
+  def team_name
+    "<bold>" + ADJECTIVES.sample + " " + NOUNS.sample + "</bold>"
+  end
+
   def text(names)
     if names.count == 2
-      "#{names.first} and #{names.last}"
+      "#{names.first} and #{names.last} are Team #{team_name}"
     elsif names.count == 1
       "#{names.first} is alone. Sorry, buddy."
     end
